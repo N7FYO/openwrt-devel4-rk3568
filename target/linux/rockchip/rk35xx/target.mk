cac95c1be934b55525750449da31650e2e32b5eb
TahomaSoft <bacon@tahomasoft.com>
2023-09-08 18:43:08 -0400

    botch

---
+++ target/linux/rockchip/rk35xx/target.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-only
+ARCH:=aarch64
+SUBTARGET:=rk35xx
+# CPU_TYPE:=
+BOARD:=rockchip
+BOARDNAME:=Rockchip RK35xx ARM Boards
+
+define Target/Description
+	Build firmware images for Rockchip RK35xx based boards.
+endef
